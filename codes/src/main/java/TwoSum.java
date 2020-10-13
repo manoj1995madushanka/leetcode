8db6bb5cb35fd284468b815a10f22fb80a3ba7ab
manoj1995madushanka <madushankamanoj414@gmail.com>
2020-10-13 22:38:22 +0530

    two sum brute force

--- codes/src/main/java/TwoSum.java
+++ codes/src/main/java/TwoSum.java
@@ -3,15 +3,9 @@ public class TwoSum {
     private static int[] getIndexes(int[] nums, int target) {
         int[] indexes = new int[2];
         for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i+1; j < nums.length; j++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    //if (nums[i] == nums[j]) {
-                    //  indexes = new int[]{j, j};
-
-                    //} else {
                     indexes = new int[]{i, j};
-
-                    //}
                 }
             }
         }
@@ -19,7 +13,7 @@ public class TwoSum {
     }
 
     public static void main(String[] args) {
-        int[] arr = {3,3};
+        int[] arr = {3, 3};
         int target = 6;
 
         int[] result = getIndexes(arr, target);
